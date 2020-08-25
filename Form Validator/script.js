cfcdfe3f367bccec85e0d333dd1b6631b1cbf6cb
Osama Ameer <osamaameer15@gmail.com>
2020-08-25 15:39:23 +0500

    Update script.js
    Added functionality to check email validation, check length, password match, confirm password

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -25,25 +25,49 @@ function showsuccess(input){
 
 
 //function to check email
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showsuccess(input);
+    } else{
+        showerror(input, `${getFieldId(input)} is not valid.`)
+    }
 }
 
 
 
 //function to validate every input field
 function checkRequired(inputArray){
-    inputArray.forEach(e => {
-        if (e.value === ''){
-            showerror(e, `${getFieldId(e)} is required`);
+    inputArray.forEach(input => {
+        if (input.value === ''){
+            showerror(input, `${getFieldId(input)} is required`);
         }
         else{
-            showsuccess(e);
+            showsuccess(input);
         }
         
     });
 
+}
+
+//function to check length
+
+function checkLength(input,min,max){
+    if(input.value.length < min){
+        showerror(input, `${getFieldId(input)} must be atleat ${min} characters.` );
+    } else if( input.value.length > max){
+        showerror(input, `${getFieldId(input)} must be less than ${max} characters.` );
+    } else {
+        showsuccess(input);
+    }
+
+}
+
+//fuction to check password and confirm password
+function checkPassMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showerror(input2, 'Password not matched!')
+    } 
 }
 
 //function to get the name of the input field
@@ -52,11 +76,17 @@ function getFieldId(input){
 }
 
 
+
 //event on sumbit button
 form.addEventListener('submit',function(e) {
     e.preventDefault();
     
 
     checkRequired([username, email, password, password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPassMatch(password, password2)
+
    
 })
