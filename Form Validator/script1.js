b16f2996d644c25c033dc28a8ee5ebb72011a21f
Osama Ameer <osamaameer15@gmail.com>
2020-08-25 15:14:07 +0500

    archeive script1.js and create new script.js
    Refactored required fileds check

--- Form Validator/script1.js
+++ Form Validator/script1.js
@@ -5,7 +5,7 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 
-//function to show errorr 
+
 function showerror(input, message){
     const formControl = input.parentElement;
        formControl.className = 'form-control error';
@@ -14,8 +14,6 @@ function showerror(input, message){
 
 }
 
-//function to show success
-
 function showsuccess(input){
     const formControl = input.parentElement;
        formControl.className = 'form-control success';
@@ -23,40 +21,40 @@ function showsuccess(input){
 
 }
 
-
-//function to check email
 function isValidEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
 
-
-//function to validate every input field
-function checkRequired(inputArray){
-    inputArray.forEach(e => {
-        if (e.value === ''){
-            showerror(e, `${getFieldId(e)} is required`);
-        }
-        else{
-            showsuccess(e);
-        }
-        
-    });
-
-}
-
-//function to get the name of the input field
-function getFieldId(input){
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-
-//event on sumbit button
 form.addEventListener('submit',function(e) {
     e.preventDefault();
     
+    if(username.value == ''){
+        showerror(username, 'Username is required!')
+    }else{
+        showsuccess(username)
+    }
 
-    checkRequired([username, email, password, password2]);
-   
+    if(email.value == ''){
+        showerror(email, 'Email is required!')
+    } else if (!isValidEmail(email.value)) {
+        showerror(email, "Email is not valid")
+    } 
+    else{
+        showsuccess(email)
+    }
+
+
+    if(password.value == ''){
+        showerror(password, 'Password is required!')
+    }else{
+        showsuccess(password)
+    }
+
+    if(password2.value == ''){
+        showerror(password2, 'Confirm Password is required!')
+    }else{
+        showsuccess(password2)
+    }
 })
