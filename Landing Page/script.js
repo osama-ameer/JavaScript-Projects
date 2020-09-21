088246d900680292814cd5e957134c6d3fd38b14
Osama Ameer <osamaameer15@gmail.com>
2020-09-21 11:42:43 +0500

    Completed Project 06
    Added animations and JS to open slider menu and modal window

---
+++ Landing Page/script.js
@@ -0,0 +1,21 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+
+//Event Listeners
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav'));
+
+//show the modal
+open.addEventListener('click', () => 
+    modal.classList.add('show-modal')
+)
+
+//close the modal
+close.addEventListener('click', ()=> 
+modal.classList.remove('show-modal'))
+
+window.addEventListener('click', e => 
+    e.target === modal ? modal.classList.remove('show-modal') : false
+)
